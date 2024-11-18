961f1c85fb2127a796eaec57190418101199e45f
Mark Doyle <mark@mdoyle.sh>
2024-11-18 13:19:51 +0000

    switch to copilot-cmp from github copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,1 +1,14 @@
-return { 'github/copilot.vim' }
+return {
+  'zbirenbaum/copilot.lua',
+  config = function()
+    require('copilot').setup {
+      panel = {
+        -- auto_refresh = true
+      },
+      suggestion = {
+        enabled = false,
+        auto_trigger = true,
+      },
+    }
+  end,
+}

73b2689cb764f6163db27bebde3afe12f47204c3
Mark Doyle <mark@mdoyle.sh>
2024-12-30 21:34:56 +0000

    add treesitter commenting to support jsx/tsx

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,1 +1,19 @@
-return { 'numToStr/Comment.nvim', opts = {} }
+return {
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = {
+      enable_autocmd = false,
+    },
+  },
+  {
+    'numToStr/Comment.nvim',
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    opts = function()
+      return {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
+  },
+}

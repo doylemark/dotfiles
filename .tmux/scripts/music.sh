eecc4ef2ea0bb0946f02241ebecce739bd4d93bd
Mark Doyle <mark@mdoyle.sh>
2025-02-27 14:12:53 +0000

    refactor music script

--- .tmux/scripts/music.sh
+++ .tmux/scripts/music.sh
@@ -1,1 +1,14 @@
-nowplaying-cli get title artist playbackRate | awk 'NR % 3 == 1 {title = $0} NR % 3 == 2 {artist = $0} NR % 3 == 0 {if ($0 == "0") print "🎧 Paused"; else printf "🎧 %s | %s\n", title, artist}'
+nowplaying-cli get title artist playbackRate | awk '
+NR % 3 == 1 {title = $0}
+NR % 3 == 2 {artist = $0}
+NR % 3 == 0 {
+    if ($0 == "0") {
+        print "🎧 Paused"
+    } else {
+        if (title == "null" && artist == "null") {
+            print "🎧 Paused"
+        } else {
+            printf "🎧 %s | %s\n", title, artist
+        }
+    }
+}'
